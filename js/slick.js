1352eee4ca13b5d9deb794504d9d649abc2d8444
Harry Klippel <harryklippel21@gmail.com>
2023-05-31 20:50:39 -0300

    Main Commit

---
+++ js/slick.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+   $('.slick-slider').slick({
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     autoplay: true,
+     arrows: false,
+     dots: true,
+     fade: true, // Usar efeito de transição de fade
+     speed: 500,
+     autoplaySpeed: 3000 // Tempo de atraso entre os slides em milissegundos
+   })
+ })
+
+ 
+
+
+ 
